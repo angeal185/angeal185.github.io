85b40be5504d9a3be1d48acf3250d0ddd6a9753b
angeal185 <beneaves01@hotmail.com>
2017-12-29 20:26:56 +0800

    update

--- app/js/app.js
+++ app/js/app.js
@@ -4,11 +4,20 @@ var wrap = ("<div id='wrapper'></div>"),
 	headerRight = ("<li id='user-header'><a href='https://github.com/angeal185/' target = '_blank'><img id='imgi'>Admin</a></li>"),
 	footer = ("<p class='sm-pull-reset shrink' style='margin:10px 80px'>2017 Ben eaves. All rights reserved.</p>"),
 	svg1 = ("<div id='svg'></div>"),
-	cdpLnk = ("<li class='cdpLnk'><a href='https://angeal185.github.io/dynamic-ajax-site-generator/' target = '_blank'>Codepen-Full</a></li>"),
 	status = "<div class='status'></div>",
 	IP = "<code id='ip'></code>",
 	toastTpl = "<ul class='toasts'></ul>";
-	
+
+
+var xtraLnk = [{
+	"li":"li:has(#j-p)",
+	"link":"https://angeal185.github.io/dynamic-ajax-site-generator/",
+	"title":"Codepen-Full"
+},{
+	"li":"li:has(#j-p)",
+	"link":"https://angeal185.github.io/animated-ajax-carosel-portfolio/",
+	"title":"Animations"
+}];
 
 //init templates
 $('body').prepend(wrap).append(toastTpl);
@@ -20,9 +29,11 @@ $('#headerRight').jsonRender(headerMainRightData,headerMain);
 //snippets
 $("body").prepend(canvas1),
 $("#usr").prepend(usr),
-$("#headerRight").append(headerRight),
-$("li:has(#j-p)").after(cdpLnk),
-$(".cdpLnk").eq(1).remove(),
+$("#headerRight").append(headerRight);
+
+$.each( xtraLnk, function( i, e ) {
+		$(e.li).not( ".nav-parent" ).after("<li class='cdpLnk'><a href='"+e.link+"' target = '_blank'>"+e.title+"</a></li>");
+});
 $("#footer").prepend(footer),
 $(".main-content").after(svg1),
 $( "#svg" ).load( "app/views/svg.tpl" );
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-﻿var static = require('node-static');
-var file = new static.Server();
+const static = require('node-static'),
+chalk = require('chalk'),
+config  = require('./config'),
+fileServer = new static.Server(config.serve, { cache: config.cache });
+
+console.log(chalk.magentaBright("Starting: ") + chalk.cyanBright(config.name));
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        file.serve(request, response);
+        fileServer.serve(request, response, function (err, result) {
+            if (err) {
+                console.log(chalk.redBright("Error serving " + request.url + " - " + err.message));
+
+                response.writeHead(err.status, err.headers);
+                response.end();
+            } else {
+              if (config.debug) {
+              console.log(chalk.magentaBright(request.method) + ' ' +
+                          chalk.greenBright(request.url) + ' ' +
+                          chalk.blueBright(result.message)
+                        );
+                      }
+            }
+        });
     }).resume();
-}).listen(8080);
+}).listen(config.port);
+console.log(chalk.greenBright("Server listening on port: ") + chalk.cyanBright(config.port));
+console.log(chalk.greenBright("Working DIR: ") + chalk.cyanBright(process.cwd()));
